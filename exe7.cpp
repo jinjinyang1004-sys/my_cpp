b8e1a29e18b1ad4249db7de374695f3e2307f75c
jinjinyang1004-sys <jinjinyang1004@gmail.com>
2026-04-19 16:11:06 +0800

    Update exe7.cpp

--- exe7.cpp
+++ exe7.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int a,b,c ;
-    cin>>a>>b>>c ;
-    cout<<(a*b*c) ;
-
+    int y,m ;
+    cin>>y>>m ;
+    switch(m){
+    case 4:case 6:case 9:case 11:
+        cout<<"30天"<<endl ;
+        break ;
+    case 2:
+        if ((((y%4)==0) && ((y%100)!=0))||((y%400)==0))
+            cout<<"29天"<<endl ;
+        else
+            cout<<"28天"<<endl ;
+        break ;
+    degault:
+        cout<<"31天"<<endl ;
+        break ;
     }
+}
